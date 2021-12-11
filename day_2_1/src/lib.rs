c0c8a4c916e92729ada1b8840e0fc31a0f3f9339
Hayden Isler <me@haydenisler.com>
2021-12-10 23:48:43 -0600

    + day 2, pt 1

---
+++ day_2_1/src/lib.rs
@@ -0,0 +1,45 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub struct Config {
+  pub filename: String,
+}
+
+impl Config {
+  pub fn new(args: &[String]) -> Result<Config, &str> {
+    if args.len() < 2 {
+      return Err("not enough arguments");
+    }
+
+    let filename = args[1].clone();
+    Ok(Config { filename })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let file = File::open(config.filename)?;
+  let reader = BufReader::new(file);
+
+  let mut x = 0;
+  let mut y = 0;
+
+  for (_, line) in reader.lines().enumerate() {
+    let line = line?;
+
+    let mut iter = line.split_whitespace();
+    let direction = iter.next().unwrap();
+    let magnitude = iter.next().unwrap().parse::<i32>().unwrap();
+
+    match direction {
+      "forward" => x = x + magnitude,
+      "up" => y = y - magnitude,
+      "down" => y = y + magnitude,
+      _ => println!("I sure hope we don't hit this!"),
+    }
+  }
+
+  println!("x * y: {}", x * y);
+
+  Ok(())
+}
